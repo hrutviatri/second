a77d76ebbd4ac62c183bd05299f61fac7b1801be
Ritesh Sharma <er.ritesh2@gmail.com>
2025-11-12 20:43:13 +0530

    Code Updated

--- infra/provider.tf
+++ infra/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  #   subscription_id = "enter-your-subscription-id-here"
+    subscription_id = "95a90f4b-7e15-47ba-95ab-0d52bac078e4"
 }
 
 terraform {
